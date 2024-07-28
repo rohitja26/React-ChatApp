1a2f0a1eb0edf2574a0f9f691d9d4ef5e6b680dc
rohitja26 <jainrohit0002@gamil.com>
2024-07-28 23:39:51 +0530

    feat/backend-updated

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,5 +1,5 @@
 const User = require("../models/usersModal");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 module.exports.register = async (req, res, next) => {
   try {
